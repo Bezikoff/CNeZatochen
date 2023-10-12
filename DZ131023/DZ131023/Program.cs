55b800d8efbdec433fbc7e09c8d47e579da48825
Bezikoff <115394053+Bezikoff@users.noreply.github.com>
2023-10-12 20:28:00 +0300

    Add files via upload

---
+++ DZ131023/DZ131023/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("при а = 1.1");
+for (var i = Convert.ToDouble(0); i < 10; i+= 0.5)
+{
+    Console.WriteLine(((i * 1.1) + Math.Log(1 + (i * 1), 10)) / (1 + Math.Pow(i * 1.1, 2)));
+}
+Console.WriteLine("при а = 1.6");
+for (var i = Convert.ToDouble(0); i < 10; i += 0.5)
+{
+    Console.WriteLine(((i * 1.6) + Math.Log(1 + (i * 1), 10)) / (1 + Math.Pow(i * 1.6, 2)));
+}
